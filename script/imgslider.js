44c722ab554792dba47dae4fb6ee352e3923cb88
Osinachi <mhiztaaustin@gmail.com>
2021-12-26 04:40:17 +0100

    commit

---
+++ script/imgslider.js
@@ -0,0 +1,39 @@
+const img = document.querySelector("#img")
+let images = ["./public/pix/ngo1.jpg","./public/pix/ngo2.jpg","./public/pix/ngo13.jpg","./public/pix/ngo7.jpg","./public/pix/ngo5.jpg","./public/pix/ngo6.jpg","./public/pix/ngo14.jpg"]
+let num = 0
+
+function transit(){
+    num++;
+    if(num<images.length){
+        img.src = images[num]
+        //alert(num)
+    }
+
+    else if(num==images.length){
+        num = 0
+        img.src = images[num]
+        //alert(num)
+    }
+}                                               
+window.setInterval(transit,2000)
+
+
+
+function next(){
+    num++;
+    if(num==images.length){
+        num = 0
+        img.src = images[num]
+        //alert(num)
+    }
+    img.src = images[num]
+
+}
+
+function prev(){
+    num--;
+    if(num==0){
+        num=0
+    }
+    img.src = images[num]
+}
---
+++ script/time.js
@@ -0,0 +1,5 @@
+let date = new Date()
+let years = date.getFullYear()
+let time = document.querySelector(".time")
+//time.innerText= years
+time.append(" "  +years)
